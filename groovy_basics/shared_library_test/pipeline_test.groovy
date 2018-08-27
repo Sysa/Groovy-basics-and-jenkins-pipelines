da64be65c9e2d385fed13d3db9b60bda4594f11c
alexkh <khlopov@Otkritie.local>
2018-08-27 17:42:24 +0300

    SCM and jobs added

--- groovy_basics/shared_library_test/pipeline_test.groovy
+++ groovy_basics/shared_library_test/pipeline_test.groovy
@@ -1,14 +1,21 @@
 @Library('shared_library_test')_
 import org.testorg.test_sources
-node {
+node('master') {
     stage ("First stage")
         {echo "Hello world"}
     }
 stage ("2")
-    {echo 'Hello World-2'}
+    {echo 'Hello World-2'
+        echo env.BUILD_ID
+        echo params.test_choise
+    }
 node {
     stage ("3")
-        {echo 'Hello-3'}
+        {
+            echo 'Hello-3'
+            def choise = getProperty('test_choise')
+            echo choise
+        }
 }
 node {
     stage ("shared")
@@ -22,4 +29,26 @@ node {
         test_src = new org.testorg.test_sources()
         test_src.call_test_source()
     }
+}
+
+node {
+	stage ("git checkout")
+	{
+		echo "SCM step"
+		//http://localhost:7990/scm/tes/mytestrepo.git
+		checkout changelog: false, poll: false, scm: [$class: 'GitSCM',
+													branches: [[name: '*/testdev']],
+													doGenerateSubmoduleConfigurations: false,
+													extensions: [], submoduleCfg: [],
+													userRemoteConfigs: [[credentialsId: 'alexkh_git_credentials',
+													url: 'http://localhost:7990/scm/tes/mytestrepo.git']]]
+		echo "Git checkout, shorthand"
+		git branch: 'testdev', changelog: false, credentialsId: 'alexkh_git_credentials', poll: false, url: 'http://localhost:7990/scm/tes/mytestrepo.git'
+	}
+}
+node {
+	stage ("run job")
+	{
+		build job: 'simple_job'
+	}
 }
