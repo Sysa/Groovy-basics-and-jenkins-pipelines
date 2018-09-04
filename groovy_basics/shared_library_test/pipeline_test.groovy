8e7d6386853d5668ab79c2d8df2d2fa4fc4e7256
Sysa <sysan1n@yandex.ru>
2018-09-04 12:32:58 +0300

    updating of basic pipeline

--- groovy_basics/shared_library_test/pipeline_test.groovy
+++ groovy_basics/shared_library_test/pipeline_test.groovy
@@ -32,6 +32,22 @@ node {
 }
 
 node {
+	stage ("cleaning workspace")
+	{
+		echo "Workspace is " + env.WORKSPACE
+		//deleteDir()
+		sleep(time:3,unit:"SECONDS")
+		//this is can be retired with `deleteDir()`
+		//clean_workspace(env.WORKSPACE)
+		//sleep(time:3,unit:"SECONDS")
+		//clean_workspace("${WORKSPACE}")
+		echo pwd()
+		dir("mytestrepo") {deleteDir()}
+		echo pwd()
+	}
+}
+
+node {
 	stage ("git checkout")
 	{
 		echo "SCM step"
@@ -51,4 +67,30 @@ node {
 	{
 		build job: 'simple_job'
 	}
+}
+
+def new_value = "bin"
+
+node {
+	stage ("pass variable")
+	{
+		pass_string_value(new_value)
+	}
+}
+
+def pass_string_value (String path_value)
+	{
+		def parameters = "/p:DefineConstants=DEBUG /p:Platform=\"Any CPU\" /p:OutputPath=$path_value"
+		echo parameters
+		//the same:
+		echo "${WORKSPACE}"
+		echo env.WORKSPACE
+	}
+
+//this func can be swapped with the `deleteDir()`, but be ahead of this one issue - https://issues.jenkins-ci.org/browse/JENKINS-41805
+def clean_workspace(String destination_folder) {
+	//def PS = "PowerShell.exe -ExecutionPolicy Bypass -Command"
+	//def cmd_command = "${PS} \"if (\$(Test-Path ${destination_folder})) {Remove-Item ${destination_folder}\\* -Recurse -Force\"}"
+	//bat cmd_command
+	powershell 'write-host "testpowershell" '
 }
